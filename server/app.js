cf7aa9ee3b35a5062dd8143b67b1ae6c1bc636d3
Eduardo San Martin Morote <posva13@gmail.com>
2018-02-23 14:32:13 +0100

    add authentication

--- server/app.js
+++ server/app.js
@@ -3,6 +3,13 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/tweeter');
+
+const passport = require('passport');
+const User = require('./models/user');
+const config = require('./config');
+const { Strategy, ExtractJwt } = require('passport-jwt');
 
 var app = express();
 
@@ -13,7 +20,47 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
+// Create the strategy for JWT
+const strategy = new Strategy(
+  {
+    // this is a config we pass to the strategy
+    // it needs to secret to decrypt the payload of the
+    // token.
+    secretOrKey: config.jwtSecret,
+    // This options tells the strategy to extract the token
+    // from the header of the request
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  },
+  (payload, done) => {
+    // payload is the object we encrypted at the route /api/token
+    // We get the user id, make sure the user exist by looking it up
+    User.findById(payload.id).then(user => {
+      if (user) {
+        // make the user accessible in req.user
+        done(null, user);
+      } else {
+        done(new Error('User not found'));
+      }
+    });
+  }
+);
+// tell pasport to use it
+passport.use(strategy);
+
 app.use('/', require('./routes/index'));
+app.use('/api', require('./routes/auth'));
+
+app.get(
+  '/api/secret',
+  // this is protecting the route and giving us access to
+  // req.user
+  passport.authenticate('jwt', config.jwtSession),
+  (req, res) => {
+    // send the user his own information
+    res.json(req.user);
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
