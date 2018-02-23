14a3f838c44d35817be04be977a992bc501536f1
Eduardo San Martin Morote <posva13@gmail.com>
2018-02-23 15:04:19 +0100

    add signup page

---
+++ client/src/views/Signup.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <form>
+      <b-field label="Name">
+          <b-input v-model="name"></b-input>
+      </b-field>
+
+      <b-field label="Username">
+          <b-input maxlength="30" required icon="account"></b-input>
+      </b-field>
+
+      <b-field label="Password">
+          <b-input type="password"
+            required
+            icon="lock"
+              password-reveal>
+          </b-input>
+      </b-field>
+      <button class="button is-primary">Signup</button>
+    </form>
+  </section>
+</template>
