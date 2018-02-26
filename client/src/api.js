86cb47ce8e3fcd1540d606f02be2e6bdabae224a
Eduardo San Martin Morote <posva13@gmail.com>
2018-02-26 09:49:34 +0100

    update api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api',
 });
 
 const errHandler = err => {
